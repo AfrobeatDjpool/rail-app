16391d748469b505aa2234cca7a7e3d464ac7f91
Arvind Kushwah <arvindkushwah9@gmail.com>
2018-05-10 13:59:37 +0530

    dded Graph

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -5,9 +5,9 @@ class RentsController < ApplicationController
   # GET /rents.json
   def index
     if params[:name]
-      @rents = Rent.search(params).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+      @rents = Rent.search(params).order("created_at DESC").paginate(:page => params[:page], :per_page => 30)
     else
-      @rents = Rent.paginate(:page => params[:page], :per_page => 10)
+      @rents = Rent.paginate(:page => params[:page], :per_page => 30)
     end
   end
 

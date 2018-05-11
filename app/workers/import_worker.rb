8b6b342a475b625d7ad85a86f740c5ca14e52c03
Arvind Kushwah <arvindkushwah9@gmail.com>
2018-05-11 21:05:56 +0530

    chnages

--- app/workers/import_worker.rb
+++ app/workers/import_worker.rb
@@ -17,8 +17,8 @@ class ImportWorker
       # csv.with_index { |row,index|
         # ["user_id", "patente", "dv_patente", "marca", "modelo", "tipo", "fecha_ult_trans", "color", "resto_color", "ano_fab", "chasis", "numero_motor", "rut", "dv", "nombre", "multas","date_of_db","created_at","updated_at"]
         
-          if index > 3297010 #1589399 - 1609438
-            
+          if index > 0 #1589399 - 1609438
+            puts "Row: #{index}"
             # inserts.push "(#{current_user.try(:id)}, #{row[0]}, #{row[1]}, #{row[2]}, #{row[3]}, #{row[4]}, #{row[5]}, #{row[6]}, #{row[7]}, #{row[8]}, #{row[9]}, #{row[10]}, #{row[11]}, #{row[12]}, #{row[13]}, #{row[14]}, #{row[15]}, '#{time}', '#{time}')"
             rent_data = {}
             rent_data["user_id"] = current_user.try(:id)

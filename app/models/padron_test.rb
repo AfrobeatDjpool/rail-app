e6dceee4a0d3b4c3ac5d58d3793fdcace9521deb
Arvind Kushwah <arvindkushwah9@gmail.com>
2018-05-18 12:57:26 +0530

    Chnages

--- app/models/padron_test.rb
+++ app/models/padron_test.rb
@@ -1,5 +1,5 @@
 class PadronTest < ApplicationRecord
-   self.table_name = "padron_test"
+   self.table_name = "xiii_region_merge"
 	def self.search(params)
 	  # Title is for the above case, the OP incorrectly had 'name'
 	  where("first_name LIKE ? OR brand LIKE ? OR date_of_db LIKE ?", "%#{params[:name]}%", "%#{params[:brand]}%", "%#{params[:date_of_db]}%")

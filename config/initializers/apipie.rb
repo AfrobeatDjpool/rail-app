bf7294f78fc449a9400564e694a226cc02bfd496
rakesh <rakeshmaurya262@gmail.com>
2019-02-13 19:50:03 +0530

    Modified Apipie

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -6,9 +6,9 @@ config.translate = false
 
   # where is your API defined?
   config.api_controllers_matcher = ["#{Rails.root}/app/controllers/*/*/*.rb", "#{Rails.root}/app/controllers/*/*.rb", "#{Rails.root}/app/controllers/*.rb"]
-  config.authenticate = Proc.new do
-    authenticate_or_request_with_http_basic do |username, password|
-      username == "admin" && password == "password"
-    end
-  end
+  # config.authenticate = Proc.new do
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == "admin" && password == "password"
+  #   end
+  # end
 end

124bf503fa7bb9711985ffba760b4e64067a2f8d
rakesh <rakeshmaurya262@gmail.com>
2019-02-15 18:01:42 +0530

    Add Route

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,8 @@ Rails.application.routes.draw do
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
-  # root 'users#show'
+  # root 'users#index'
+  root 'users#show'
   namespace :api do
 	  namespace :v1 do
 	  	resources :users, only: [:index] 

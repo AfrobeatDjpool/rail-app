389a3a3d0a989b35995e2aee988db0da45a877ec
arvind kushwah <arvindkushwah9@gmail.com>
2019-03-08 19:29:23 +0530

    Add Song Download api

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -34,7 +34,7 @@ class Api::V1::SongsController < Api::V1::ApiController
     # if current_user.role == "admin"
       # user = current_user.first_name
       # song = current_user.new(admin_song_params)
-      song = Song.new(name_song: params[:song][:name_song], image: params[:image], user_id: params[:song][:user_id], beats: params[:song][:beats], genre: params[:song][:genre], version: params[:song][:version], name_artist: params[:song][:name_artist], date_uploaded: params[:song][:date_uploaded])
+      song = Song.new(name_song: params[:song][:name_song], image: params[:image], user_id: params[:song][:user_id], beats: params[:song][:beats], genre: params[:song][:genre], version: params[:song][:version], name_artist: params[:song][:name_artist],url: params[:song][:url], date_uploaded: params[:song][:date_uploaded])
       # song.user_id = current_user.try(:id)
       if params[:image].present?
         @audio = SongAudio.find(params[:image])
@@ -74,12 +74,15 @@ class Api::V1::SongsController < Api::V1::ApiController
   
   def show
     @song = Song.find(params[:id])
+    # song_audio = SongAudio.find_or_initialize_by(song_id: @song.id)
+    # name = song_audio.name
+    url = "http://localhost:3000"+ @song.song_audio.name.url
     name_song = @song.name_song
     name_artist = @song.name_artist
     date_uploaded = @song.date_uploaded
     artist_upload_song = @song.user.first_name
     # render json: {status: 200, data: {:name_song => name_song, :name_artist => name_artist, :date_uploaded => date_uploaded, :artist_upload_song => artist_upload_song}, :message =>"Show Song Details"}
-    render json: {status: 200, data: { :song => @song.as_json}, :message =>"Show Song Details"}
+    render json: {status: 200, data: { :song => @song.as_json, :url => url}, :message =>"Show Song Details"}
   end
 
   def download_section
@@ -89,6 +92,12 @@ class Api::V1::SongsController < Api::V1::ApiController
     name_artist = @song.name_artist
     date_uploaded = @song.date_uploaded
     render json: {status: 200, data: { :songs => @songs.as_json, :name_song => name_song, :name_artist => name_artist, :date_uploaded => date_uploaded}, :message =>"Download Song"}
+  end
+
+  def download
+    @song = Song.find(params[:id])
+     url = "http://localhost:3000"+ @song.song_audio.name.url
+    render json: {status: 200, data: { :song => @song.as_json, :url => url }, :message =>"Song Download"}
   end
 
   eval(IO.read('doc/api_doc/songs/delete.html'), binding)
@@ -142,7 +151,7 @@ class Api::V1::SongsController < Api::V1::ApiController
 
       # Never trust parameters from the scary internet, only allow the white list through.
       def admin_song_params
-        params.require(:admin_song).permit(:user_id, :name_song, :beats, :genre, :version, :name_artist, :image, :date_uploaded)
+        params.require(:admin_song).permit(:user_id, :name_song, :beats, :genre, :version, :name_artist, :image, :date_uploaded, :url)
       end
   
 
